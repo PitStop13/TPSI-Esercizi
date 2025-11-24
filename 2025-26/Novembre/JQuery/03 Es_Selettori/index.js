d0bc5d961bb0249ba1f22cfe41c8cd0bf8fe6db3
PitStop13 <p.olivero.3584@vallauri.edu>
2025-11-24 09:09:55 +0100

    Corretto bug ultimo esercizio jquery

--- 2025-26/Novembre/JQuery/03 Es_Selettori/index.js
+++ 2025-26/Novembre/JQuery/03 Es_Selettori/index.js
@@ -13,20 +13,18 @@ $(document).ready(function () {
 
     _countP.click(function () {
         $("#output").text($("p").length);
-
     });
 
     _colorDiv.click(function () {
-        $("#div").addClass("evidenziato");
-
+        $(".box").addClass("evidenziato");
+        $("#output").text("Evidenziato");
     });
     _showSpecial.click(function () {
         $("#output").text($("#speciale").text());
     });
     _reset.click(function () {
-        $("#output").text("Results...");
-        $("div").removeClass("evidenziato");
+        $("#output").text("Risultati...");
+        $(".box").removeClass("evidenziato");
     });
-
 
 });
