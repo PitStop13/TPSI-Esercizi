9ec77c289f6d8fd9027b1bea66c2d1a539bff1f2
PitStop13 <p.olivero.3584@vallauri.edu>
2025-11-24 11:09:22 +0100

    Svolto esercizio Immagini

---
+++ 2025-26/Novembre/JQuery/04 Es_Visualizzatore_Immagini/index.js
@@ -0,0 +1,59 @@
+"use strict";
+
+$(document).ready(function(){
+   let nImmagine=1;
+
+   $("#btnIndietro").css({
+      backgroundColor:"orange",
+      width:"160px",
+      height:"40px",
+      textAlign:"center",
+      borderRadius:"100%",
+      fontWeight:"bold",
+      margin:"20px 10px"
+   }).prop("disabled",true);
+
+   $("#btnAvanti").css({
+      backgroundColor:"orange",
+      width:"160px",
+      height:"40px",
+      textAlign: "center",
+      borderRadius:"100%",
+      fontWeight: "bold",
+      margin:"20px 10px"
+   });
+
+   $("#img").prop("src","img/img1.jpg").css(
+       {
+          width:"400px",
+          height:"400px",
+          verticalAlign:"middle"
+       }
+   );
+
+   $("#btnIndietro").click(function(){
+      nImmagine--;
+      $("#img").fadeOut(500,function(){
+         $("#img").prop("src","img/img"+nImmagine+".jpg");
+         $("#img").fadeIn(500);
+      });
+      if(nImmagine==1)
+      {
+         $("#btnIndietro").prop("disabled",true);
+      }
+      $("#btnAvanti").prop("disabled",false);
+   });
+
+   $("#btnAvanti").click(function(){
+      nImmagine++;
+      $("#img").fadeOut(500,function(){
+         $("#img").prop("src","img/img"+nImmagine+".jpg");
+         $("#img").fadeIn(500);
+      });
+      if(nImmagine==7)
+      {
+         $("#btnAvanti").prop("disabled",true);
+      }
+      $("#btnIndietro").prop("disabled",false);
+   });
+});
