98c9d6124af2f0ce6792b0d5d39c165201a4eba4
PitStop13 <p.olivero.3584@vallauri.edu>
2025-12-15 09:41:29 +0100

    Modificato per funzionare con jQuery,da implementare l'animazione

--- 2025-26/Dicembre/JQuery/10_Esercitazione2/index.js
+++ 2025-26/Dicembre/JQuery/10_Esercitazione2/index.js
@@ -1,70 +1,75 @@
 const DIM = 4;
-window.onload = function () {
-    let wrapper = document.getElementById("wrapper");
+$(document).ready(function () {
+    let wrapper = $("#wrapper");
     for (let i = 0; i < DIM; i++) {
         for (let j = 0; j < DIM; j++) {
-            let div = document.createElement("div");
-            div.id = "btn" + i + j;
-            div.addEventListener("click", sposta); //sposta SENZA le tonde dato che altrimenti vuool dire che la chiami
-            wrapper.appendChild(div);
+            let div = $("<div></div>");
+            div.attr("id", "btn" + i + j);
+            div.addClass("btn"); // Optional: add a class for styling if needed, though id is used for logic
+            div.click(sposta);
+            wrapper.append(div);
         }
 
     }
-    //devo capire meglio come
+
     let ref;
     for (let i = 1; i < 16; i++) {
         let x, y;
         do {
             x = Math.floor(DIM * Math.random()); //0-3
             y = Math.floor(DIM * Math.random()); //0-3
-            ref = document.getElementById("btn" + x + y);
-        } while (ref.innerText != "");
-        ref.innerText = i;
+            ref = $("#btn" + x + y);
+        } while (ref.text() != "");
+        ref.text(i);
 
     }
-}
+});
 
 function sposta() {
-    let btn = this;
-    let x = parseInt(btn.id.substr(3, 1));
-    let y = parseInt(btn.id.substr(4, 1));
+    let btn = $(this);
+    let id = btn.attr("id");
+    let x = parseInt(id.substr(3, 1));
+    let y = parseInt(id.substr(4, 1));
 
-    if (y > 0 && document.getElementById("btn" + x + (y - 1)).innerText == "") {
-        swap(btn, document.getElementById("btn" + x + (y - 1)));
+    if (y > 0 && $("#btn" + x + (y - 1)).text() == "") {
+        swap(btn, $("#btn" + x + (y - 1)));
+    } else if (y < DIM - 1 && $("#btn" + x + (y + 1)).text() == "") {
+        swap(btn, $("#btn" + x + (y + 1)));
+    } else if (x > 0 && $("#btn" + (x - 1) + y).text() == "") {
+        swap(btn, $("#btn" + (x - 1) + y));
+    } else if (x < DIM - 1 && $("#btn" + (x + 1) + y).text() == "") {
+        swap(btn, $("#btn" + (x + 1) + y));
     }
 
-    if (y < DIM - 1 && document.getElementById("btn" + x + (y + 1)).innerText == "") {
-        swap(btn, document.getElementById("btn" + x + (y + 1)));
-    }
-
-    if (x > 0 && document.getElementById("btn" + (x-1) + y).innerText == "") {
-        swap(btn, document.getElementById("btn" + (x-1) + y));
-    }
-
-    if (x < DIM -1 && document.getElementById("btn" + (x+1) + y).innerText == "") {
-        swap(btn, document.getElementById("btn" + (x+1) + y));
-    }
     let indice = 1;
-    for(let righe = 0;righe<DIM;righe++){
-        for(let colonne = 0;colonne<DIM;colonne++){
-            let bottone = document.getElementById("btn" + righe + colonne);
-            if(bottone.innerText!=indice){
-                if(indice == 16 && bottone.innerText == ""){
-                    alert("Hai vinto!")
+    for (let righe = 0; righe < DIM; righe++) {
+        for (let colonne = 0; colonne < DIM; colonne++) {
+            let bottone = $("#btn" + righe + colonne);
+            if (bottone.text() != indice) {
+                if (indice == 16 && bottone.text() == "") {
+                    alert("Hai vinto!");
                 }
                 return;
-
             }
-            
-            indice++
+
+            indice++;
         }
     }
-    
+
 }
 
 function swap(Pieno, Vuoto) {
-    let tmp = Pieno.innerText;
-    Pieno.innerText = Vuoto.innerText;
-    Vuoto.innerText = tmp; //Scambio il contenuto tra le due celle adiacenti
-    
+    let tmp = Pieno.text();
+    Pieno.text(Vuoto.text());
+    Vuoto.text(tmp);
+    //aggiungo un animazione per lo scambio carino che le faccia cambiare colore
+    //Uso animate per farli passare dalla posizione di partenza alla posizione di arrivo
+    Pieno.animate({
+        left: Vuoto.position().left,
+        top: Vuoto.position().top
+    }, 1000);
+    Vuoto.animate({
+        left: Pieno.position().left,
+        top: Pieno.position().top
+    }, 1000);
 }
