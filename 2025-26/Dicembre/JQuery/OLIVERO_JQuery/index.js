9dc17dad33e7d74d95f7b56149dd32fcb36a4d38
PitStop13 <p.olivero.3584@vallauri.edu>
2026-01-28 10:01:55 +0100

    Verifica corretta,completa e funzionante

--- 2025-26/Dicembre/JQuery/OLIVERO_JQuery/index.js
+++ 2025-26/Dicembre/JQuery/OLIVERO_JQuery/index.js
@@ -2,159 +2,148 @@
 $(document).ready(function () {
     $("<h1>").prop("id", "Titolo").text("Gestione Scaffali Magazzino").appendTo("body");
 
-    // $("<select>").prop("id", "selezione").appendTo("body");
-    // $("select").append($("<option>").val("0").text("Seleziona Operazione"));
-    // $("select").append($("<option>").val("1").text("aggiungi pacco a scaffale A"));
-    // $("select").append($("<option>").val("2").text("aggiungi pacco a scaffale B"));
-    // $("select").append($("<option>").val("3").text("Sposta Ultimo pacco da A a B"));
-    // $("select").append($("<option>").val("4").text("Sposta Ultimo pacco da B a A"));
-    // $("select").append($("<option>").val("5").text("Inserisci pacco in testa a A"));
-    // $("select").append($("<option>").val("6").text("Inserisci pacco dopo il primo B"));
-    // $("select").append($("<option>").val("7").text("Duplica pacchi scaffale A"));
-    // $("select").append($("<option>").val("8").text("Duplica pacchi scaffale B"));
-    // $("select").append($("<option>").val("9").text("Evidenzia pacchi PARI"));
-    // $("select").append($("<option>").val("10").text("Evidenzia pacchi DISPARI"));
-    // $("select").append($("<option>").val("11").text("Svuota scaffale A"));
-    // $("select").append($("<option>").val("12").text("Svuota scaffale B"));
-    // $("select").append($("<option>").val("13").text("Conta pacchi totali"));
-    // $("select").append($("<option>").val("14").text("Ordina pacchi scaffale A"));
-    // $("select").append($("<option>").val("15").text("Ordina pacchi scaffale B"));
+    let $container = $("<div>").prop("id", "selezione-container").appendTo("body");
+    $container.append($("<h3>").text("Seleziona un'operazione"));
 
-    $("<div>").prop("id", "selezione").appendTo("body");
-    $("#selezione").append($("<h4>").prop("id", "0").text("Seleziona Operazione"));
-    $("#selezione").append($("<button>").prop("id", "1").text("aggiungi pacco a scaffale A"));
-    $("#selezione").append($("<button>").prop("id", "2").text("aggiungi pacco a scaffale B"));
-    $("#selezione").append($("<button>").prop("id", "3").text("Sposta Ultimo pacco da A a B"));
-    $("#selezione").append($("<button>").prop("id", "4").text("Sposta Ultimo pacco da B a A"));
-    $("#selezione").append($("<button>").prop("id", "5").text("Inserisci pacco in testa a A"));
-    $("#selezione").append($("<button>").prop("id", "6").text("Inserisci pacco dopo il primo B"));
-    $("#selezione").append($("<button>").prop("id", "7").text("Duplica pacchi scaffale A"));
-    $("#selezione").append($("<button>").prop("id", "8").text("Duplica pacchi scaffale B"));
-    $("#selezione").append($("<button>").prop("id", "9").text("Evidenzia pacchi PARI"));
-    $("#selezione").append($("<button>").prop("id", "10").text("Evidenzia pacchi DISPARI"));
-    $("#selezione").append($("<button>").prop("id", "11").text("Svuota scaffale A"));
-    $("#selezione").append($("<button>").prop("id", "12").text("Svuota scaffale B"));
-    $("#selezione").append($("<button>").prop("id", "13").text("Conta pacchi totali"));
-    $("#selezione").append($("<button>").prop("id", "14").text("Ordina pacchi scaffale A"));
-    $("#selezione").append($("<button>").prop("id", "15").text("Ordina pacchi scaffale B"));
+    let $select = $("<select>").prop("id", "selezione").appendTo($container);
+    $select.append($("<option>").val("0").text("--- Scegli Operazione ---"));
+    $select.append($("<option>").val("1").text("Aggiungi pacco a scaffale A"));
+    $select.append($("<option>").val("2").text("Aggiungi pacco a scaffale B"));
+    $select.append($("<option>").val("3").text("Sposta Ultimo pacco da A a B"));
+    $select.append($("<option>").val("4").text("Sposta Ultimo pacco da B a A"));
+    $select.append($("<option>").val("5").text("Inserisci pacco in testa a A"));
+    $select.append($("<option>").val("6").text("Inserisci pacco dopo il primo B"));
+    $select.append($("<option>").val("7").text("Duplica pacchi scaffale A"));
+    $select.append($("<option>").val("8").text("Duplica pacchi scaffale B"));
+    $select.append($("<option>").val("9").text("Evidenzia pacchi PARI"));
+    $select.append($("<option>").val("10").text("Evidenzia pacchi DISPARI"));
+    $select.append($("<option>").val("11").text("Svuota scaffale A"));
+    $select.append($("<option>").val("12").text("Svuota scaffale B"));
+    $select.append($("<option>").val("13").text("Conta pacchi totali"));
+    $select.append($("<option>").val("14").text("Ordina pacchi scaffale A"));
+    $select.append($("<option>").val("15").text("Ordina pacchi scaffale B"));
 
-    //Creo gli scaffali
+    // Creo gli scaffali
     let cont = 0;
 
     $("<div>").prop("id", "area").appendTo("body");
-    $("#area").append($("<div>").addClass("scaffale").prop("id", "scaffale1"));
-    $("#area").append($("<div>").addClass("scaffale").prop("id", "scaffale2"));
+
+    let $scaf1 = $("<div>").addClass("scaffale").prop("id", "scaffale1").appendTo("#area");
+    $scaf1.append($("<h3>").text("Scaffale A"));
+
+    let $scaf2 = $("<div>").addClass("scaffale").prop("id", "scaffale2").appendTo("#area");
+    $scaf2.append($("<h3>").text("Scaffale B"));
+
+    function getRandomColor() {
+        const h = Math.floor(Math.random() * 360);
+        const s = Math.floor(Math.random() * 30) + 60; // 60-90% saturation
+        const l = Math.floor(Math.random() * 20) + 40; // 40-60% lightness (better for white text)
+        return `hsl(${h}, ${s}%, ${l}%)`;
+    }
+
+    function createPacco() {
+        cont++;
+        return $("<div>")
+            .addClass("pacco")
+            .text("PK-" + cont)
+            .css("background-color", getRandomColor());
+    }
+
+    // Inizializzazione
     for (let i = 0; i < 4; i++) {
-        cont++;
-        $("#scaffale1").append($("<div>").addClass("pacco").text("PK-" + cont));
-    }
-    for (let j = 0; j < 4; j++) {
-        cont++;
-        $("#scaffale2").append($("<div>").addClass("pacco").text("PK-" + cont));
+        $scaf1.append(createPacco());
+        $scaf2.append(createPacco());
     }
 
-    $("#1").on("click", function () {
-        cont++
-        $("#scaffale1").append($("<div>").addClass("pacco").text("PK-" + cont));
+    $select.on("change", function () {
+        let op = $(this).val();
 
-    })
+        // Rimuovo evidenziazioni precedenti per pulizia
+        $(".pacco").removeClass("highlight").css("border", "none");
 
-    $("#2").on("click", function () {
-        cont++
-        $("#scaffale2").append($("<div>").addClass("pacco").text("PK-" + cont));
-    })
-
-    $("#3").on("click", function () {
-        $("#scaffale2").prepend(nuovoDiv); // Mette all'inizio
-        let temp = $("#scaffale1").children().last();
-        $("#scaffale1").children().eq(0) = temp;
-
-    })
-
-    $("#4").on("click", function () {
-        console.log("1")
-    })
-
-    $("#5").on("click", function () {
-        cont++;
-        $("#scaffale1").prepend($("<div>").addClass("pacco").text("PK-" + cont)); // Mette all'inizio
-    })
-
-    $("#6").on("click", function () {
-        console.log("1")
-    })
-
-    $("#7").on("click", function () {
-
-        let cont1 = $("#scaffale1").children().get();
-
-        for (let i = 0; i < cont1; i++) {
-            cont++;
-            $("#scaffale1").append($("<div>").addClass("pacco").text("PK-" + cont));
+        switch (op) {
+            case "1": // Aggiungi pacco a scaffale A
+                $scaf1.append(createPacco());
+                break;
+            case "2": // Aggiungi pacco a scaffale B
+                $scaf2.append(createPacco());
+                break;
+            case "3": // Sposta Ultimo pacco da A a B
+                let lastA = $scaf1.find(".pacco").last();
+                if (lastA.length) $scaf2.append(lastA);
+                break;
+            case "4": // Sposta Ultimo pacco da B a A
+                let lastB = $scaf2.find(".pacco").last();
+                if (lastB.length) $scaf1.append(lastB);
+                break;
+            case "5": // Inserisci pacco in testa a A
+                $scaf1.find("h3").after(createPacco());
+                break;
+            case "6": // Inserisci pacco dopo il primo B
+                let firstB = $scaf2.find(".pacco").first();
+                if (firstB.length) {
+                    firstB.after(createPacco());
+                } else {
+                    $scaf2.append(createPacco());
+                }
+                break;
+            case "7": // Duplica pacchi scaffale A
+                let pacchiA = $scaf1.find(".pacco").length;
+                for (let i = 0; i < pacchiA; i++) {
+                    $scaf1.append(createPacco());
+                }
+                break;
+            case "8": // Duplica pacchi scaffale B
+                let pacchiB = $scaf2.find(".pacco").length;
+                for (let i = 0; i < pacchiB; i++) {
+                    $scaf2.append(createPacco());
+                }
+                break;
+            case "9": // Evidenzia pacchi PARI
+                $(".pacco").each(function () {
+                    let num = parseInt($(this).text().split("-")[1]);
+                    if (num % 2 === 0) $(this).addClass("highlight");
+                });
+                break;
+            case "10": // Evidenzia pacchi DISPARI
+                $(".pacco").each(function () {
+                    let num = parseInt($(this).text().split("-")[1]);
+                    if (num % 2 !== 0) $(this).addClass("highlight");
+                });
+                break;
+            case "11": // Svuota scaffale A
+                $scaf1.find(".pacco").remove();
+                break;
+            case "12": // Svuota scaffale B
+                $scaf2.find(".pacco").remove();
+                break;
+            case "13": // Conta pacchi totali
+                let total = $(".pacco").length;
+                alert("Numero totale di pacchi nel magazzino: " + total);
+                break;
+            case "14": // Ordina pacchi scaffale A
+                sortShelf($scaf1);
+                break;
+            case "15": // Ordina pacchi scaffale B
+                sortShelf($scaf2);
+                break;
         }
 
+        // Resetta la select dopo l'operazione
+        $(this).val("0");
+    });
 
-    })
-
-    $("#8").on("click", function () {
-        let cont2 = $("#scaffale2").children().get();
-
-        for (let i = 0; i < cont2; i++) {
-            cont++;
-            $("#scaffale2").append($("<div>").addClass("pacco").text("PK-" + cont));
-        }
-    })
-
-    $("#9").on("click", function () {
-        evidenzia('.pacco:nth-child(even)')
-    })
-
-    $("#10").on("click", function () {
-        evidenzia('.pacco:nth-child(odd)')
-    })
-
-    $("#11").on("click", function () {
-        $("#scaffale1").empty()
-    })
-
-    $("#12").on("click", function () {
-        $("#scaffale2").empty()
-    })
-
-    $("#13").on("click", function () {
-        alert("Numero di pacchi: " + cont);
-    })
-
-    $("#14").on("click", function () {
-        let scaf1 = $("#scaffale1").children().get()
-
-        scaf1.sort(function (a, b) {
-            return (parseInt($(a).text().split("-")[1]) - parseInt($(b).text().split("-")[1]));
+    function sortShelf($shelf) {
+        let items = $shelf.find(".pacco").get();
+        items.sort(function (a, b) {
+            let valA = parseInt($(a).text().split("-")[1]);
+            let valB = parseInt($(b).text().split("-")[1]);
+            return valA - valB;
         });
 
-        $("#scaffale1").empty().append(scaf1);
-    })
-
-    $("#15").on("click", function () {
-        let scaf2 = $("#scaffale2").children().get()
-
-        scaf2.sort(function (a, b) {
-            return (parseInt($(a).text().split("-")[1]) - parseInt($(b).text().split("-")[1]));
-        });
-
-        $("#scaffale2").empty().append(scaf2);
-    })
-
-    function evidenzia(par) {
-
-        $("#scaffale1").children(par).css("background-color", "yellow");
-        $("#scaffale2").children(par).css("background-color", "yellow");
-
+        // Rimuovo i pacchi e li reinserisco dopo l'H3
+        let $h3 = $shelf.find("h3");
+        $shelf.find(".pacco").remove();
+        $shelf.append(items);
     }
-
-    //Provo a recuperare il value
-    // $("#selezione").on("change",function(){
-    //     console.log($(this).val());
-    // });
-
 });
