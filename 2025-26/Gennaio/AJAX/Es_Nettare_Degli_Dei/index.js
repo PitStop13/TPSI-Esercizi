82da877732ee4ab400395895c00f2c23231119e7
PitStop13 <p.olivero.3584@vallauri.edu>
2026-02-12 10:21:45 +0100

    update index.js

--- 2025-26/Gennaio/AJAX/Es_Nettare_Degli_Dei/index.js
+++ 2025-26/Gennaio/AJAX/Es_Nettare_Degli_Dei/index.js
@@ -1,19 +1,30 @@
 const URL = "https://api.openbrewerydb.org/v1/breweries";
 
+let tabella = $("#table-body");
+let select = $("#select-tipologia");
+
 $(document).ready(function () {
-    let tabella = "#table-body";
-    let select = "#select-tipologia";
     let request = inviaRichiesta("GET", URL);
     request.fail(errore);
     request.done(function (data) {
         console.log(data);
         visualizzaTabella(data, tabella);
         popolaSelect(data, select);
+        select.change(function () {
+
+            let tipologia = select.val();
+            // let filteredData = data.filter(function (item) {
+            //     return item.brewery_type == tipologia;
+            // });
+            //oppure
+            let filteredData = data.filter(item => item.brewery_type == tipologia);
+            visualizzaTabella(filteredData, tabella);
+        });
     })
 })
 
 function visualizzaTabella(data, tabella) {
-    $(tabella).empty();
+    tabella.empty();
     for (let i = 0; i < data.length; i++) {
         let riga = "<tr>";
         riga += "<td>" + data[i].name + "</td>";
@@ -23,16 +34,16 @@ function visualizzaTabella(data, tabella) {
         riga += "<td>" + data[i].country + "</td>";
         riga += "<td>" + data[i].website_url + "</td>";
         riga += "</tr>";
-        $(tabella).append(riga);
+        tabella.append(riga);
     }
 }
 
 function popolaSelect(data, select) {
-    $(select).empty();
+    select.empty();
     for (let i = 0; i < data.length; i++) {
-        if ($(select).find("option[value='" + data[i].brewery_type + "']").length == 0) {
+        if (select.find("option[value='" + data[i].brewery_type + "']").length == 0) {
             let opzione = "<option value='" + data[i].brewery_type + "'>" + data[i].brewery_type + "</option>";
-            $(select).append(opzione);
+            select.append(opzione);
         }
     }
 }
