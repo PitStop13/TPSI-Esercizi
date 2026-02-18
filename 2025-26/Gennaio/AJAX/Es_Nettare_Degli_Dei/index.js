b74030d52ae3d810395cde0d3dc69710cda64bc3
PitStop13 <p.olivero.3584@vallauri.edu>
2026-02-18 09:36:19 +0100

    Finito caricamento con API,da aggiungere grafici ma devo ancora imparare a farli

--- 2025-26/Gennaio/AJAX/Es_Nettare_Degli_Dei/index.js
+++ 2025-26/Gennaio/AJAX/Es_Nettare_Degli_Dei/index.js
@@ -1,49 +1,110 @@
 const URL = "https://api.openbrewerydb.org/v1/breweries";
 
-let tabella = $("#table-body");
-let select = $("#select-tipologia");
-
 $(document).ready(function () {
-    let request = inviaRichiesta("GET", URL);
-    request.fail(errore);
-    request.done(function (data) {
-        console.log(data);
-        visualizzaTabella(data, tabella);
-        popolaSelect(data, select);
-        select.change(function () {
+    let container = $("#wrapper");
 
-            let tipologia = select.val();
-            // let filteredData = data.filter(function (item) {
-            //     return item.brewery_type == tipologia;
-            // });
-            //oppure
-            let filteredData = data.filter(item => item.brewery_type == tipologia);
-            visualizzaTabella(filteredData, tabella);
+    // --- Creazione Dinamica UI ---
+
+    // Header
+    let header = $("<header>").addClass("text-center mb-5").appendTo(container);
+    $("<h1>").addClass("display-4 font-weight-bold mb-3")
+        .text("Benvenuti nella lista dei birrifici migliori del mondo!")
+        .appendTo(header);
+    $("<div>").addClass("divider mx-auto mb-4").appendTo(header);
+    $("<p>").addClass("lead text-muted")
+        .text("Seleziona la tipologia di birra che ti interessa:")
+        .appendTo(header);
+
+    let row = $("<div>").addClass("row justify-content-center").appendTo(header);
+    let col = $("<div>").addClass("col-md-6 col-lg-4").appendTo(row);
+    let select = $("<select>").attr("id", "select-tipologia")
+        .addClass("custom-select custom-select-lg shadow-sm")
+        .appendTo(col);
+    $("<option>").val("").text("Scegli una tipologia...").prop("selected", true).appendTo(select);
+
+    // Tabella
+    let section = $("<section>").addClass("results-section mb-5").appendTo(container);
+    let card = $("<div>").addClass("card shadow border-0 overflow-hidden").appendTo(section);
+    let tableResp = $("<div>").addClass("table-responsive").appendTo(card);
+    let table = $("<table>").addClass("table table-hover mb-0").appendTo(tableResp);
+    let thead = $("<thead>").addClass("thead-dark").appendTo(table);
+    let trHead = $("<tr>").appendTo(thead);
+
+    let headers = ["NAME", "BREWERY_TYPE", "CITY", "STATE_PROVINCE", "COUNTRY", "WEBSITE_URL"];
+    headers.forEach(text => {
+        $("<th>").text(text).appendTo(trHead);
+    });
+
+    let tabella = $("<tbody>").attr("id", "table-body").appendTo(table);
+
+    // --- Logica Dati con LocalStorage ---
+    let localData = localStorage.getItem("birrifici");
+
+    if (localData) {
+        console.log("Dati caricati da localStorage");
+        let data = JSON.parse(localData);
+        setupUI(data, tabella, select);
+    } else {
+        console.log("Dati non trovati, invio richiesta API...");
+        let request = inviaRichiesta("GET", URL);
+        request.fail(errore);
+        request.done(function (data) {
+            localStorage.setItem("birrifici", JSON.stringify(data));
+            setupUI(data, tabella, select);
         });
-    })
-})
+    }
+
+});
+
+function setupUI(data, tabella, select) {
+    visualizzaTabella(data, tabella);
+    popolaSelect(data, select);
+
+    select.change(function () {
+        // Rileggiamo sempre dal localStorage
+        let dataFromStorage = JSON.parse(localStorage.getItem("birrifici"));
+        let tipologia = select.val();
+
+        if (tipologia === "") {
+            visualizzaTabella(dataFromStorage, tabella);
+        } else {
+            let filteredData = dataFromStorage.filter(item => item.brewery_type == tipologia);
+            visualizzaTabella(filteredData, tabella);
+        }
+    });
+}
 
 function visualizzaTabella(data, tabella) {
     tabella.empty();
     for (let i = 0; i < data.length; i++) {
-        let riga = "<tr>";
-        riga += "<td>" + data[i].name + "</td>";
-        riga += "<td>" + data[i].brewery_type + "</td>";
-        riga += "<td>" + data[i].city + "</td>";
-        riga += "<td>" + data[i].state_province + "</td>";
-        riga += "<td>" + data[i].country + "</td>";
-        riga += "<td>" + data[i].website_url + "</td>";
-        riga += "</tr>";
+        let riga = $("<tr>");
+        $("<td>").text(data[i].name).appendTo(riga);
+        $("<td>").text(data[i].brewery_type).appendTo(riga);
+        $("<td>").text(data[i].city).appendTo(riga);
+        $("<td>").text(data[i].state_province).appendTo(riga);
+        $("<td>").text(data[i].country).appendTo(riga);
+
+        let website = data[i].website_url;
+        if (website) {
+            let link = $("<a>").attr("href", website).attr("target", "_blank").addClass("btn-website").text("Sito web");
+            $("<td>").append(link).appendTo(riga);
+        } else {
+            $("<td>").text("").appendTo(riga);
+        }
+
         tabella.append(riga);
     }
 }
 
+/**
+ * Popola la select con le tipologie di birrificio presenti nei dati, evitando duplicati.
+ * Utilizzo un Set per l'univocità come suggerito.
+ */
 function popolaSelect(data, select) {
-    select.empty();
-    for (let i = 0; i < data.length; i++) {
-        if (select.find("option[value='" + data[i].brewery_type + "']").length == 0) {
-            let opzione = "<option value='" + data[i].brewery_type + "'>" + data[i].brewery_type + "</option>";
-            select.append(opzione);
-        }
-    }
+    // Estraiamo tutti i tipi, rimuoviamo i null/undefined e creiamo un Set per l'univocità
+    const tipologieUniche = [...new Set(data.map(item => item.brewery_type).filter(Boolean))];
+
+    tipologieUniche.forEach(type => {
+        select.append($("<option>").val(type).text(type));
+    });
 }
